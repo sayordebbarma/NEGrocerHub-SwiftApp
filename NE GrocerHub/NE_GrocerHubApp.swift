46ac6e17c802c75cfbf6dfeafa9948f144549ff1
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-10-01 18:48:17 +0530

    Initial Commit

---
+++ NE GrocerHub/NE_GrocerHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  NE_GrocerHubApp.swift
+//  NE GrocerHub
+//
+//  Created by Sayor Debbarma on 01/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NE_GrocerHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
